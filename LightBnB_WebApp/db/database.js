526564e31caffc7582a802333ca0a41b33e1ebdc
Lance <lanceirvingmanzanal@icloud.com>
2024-03-17 17:41:28 -0600

    refactored the getAllReservations function to use the lightbnb database that i created

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -78,7 +78,20 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT *
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
 };
 
 /// Properties
