c3f655713d2ecdc2a8e25152001912ec68ce7762
Lance <lanceirvingmanzanal@icloud.com>
2024-03-18 16:38:08 -0600

    refactor the addProperty function to insert a new property to the database

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -17,8 +17,7 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  return pool
-    .query('SELECT * FROM users WHERE email = $1', [email])
+  return pool.query('SELECT * FROM users WHERE email = $1', [email])
     .then(res => {
       if (res.rows.length > 0) {
         return res.rows[0];
@@ -36,8 +35,7 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return pool
-    .query('SELECT * FROM users WHERE id = $1', [id])
+  return pool.query('SELECT * FROM users WHERE id = $1', [id])
     .then(res => {
       if (res.rows.length > 0) {
         return res.rows[0];
@@ -57,13 +55,9 @@ const getUserWithId = function (id) {
 const addUser = function (user) {
   const { name, email, password } = user;
 
-  return pool
-    .query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)RETURNING *', [name, email, password])
+  return pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)RETURNING *', [name, email, password])
     .then(res => {
-      if (res.rows.length > 0) {
-        return res.rows[0];
-      }
-      return null;
+      return res.rows[0];
     })
     .catch(err => {
       console.log(err.message);
@@ -78,8 +72,7 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool
-    .query(`SELECT properties.*, reservations.*, avg(property_reviews.rating) AS average_rating
+  return pool.query(`SELECT properties.*, reservations.*, avg(property_reviews.rating) AS average_rating
   FROM reservations
   JOIN properties ON reservations.property_id = properties.id
   JOIN property_reviews ON properties.id = property_reviews.property_id
@@ -138,7 +131,7 @@ const getAllProperties = function (options, limit = 10) {
   queryString += `
    GROUP BY properties.id
   `;
-  
+
   // Filter by minimum rating
   if (options.minimum_rating) {
     queryParams.push(options.minimum_rating);
@@ -168,10 +161,17 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const { owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms } = property;
+
+  const values = [ owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms ]
+
+  return pool.query('INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;', values)
+  .then(res => {
+    return res.rows[0];
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
 };
 
 module.exports = {
